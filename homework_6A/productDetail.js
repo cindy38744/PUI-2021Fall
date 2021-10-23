c0e0bed26e16ab7cf9ed3c3cb35455634aeb274b
leilahuang0714 <chihyunh@andrew.cmu.edu>
2021-10-23 18:49:16 -0400

    new

---
+++ homework_6A/productDetail.js
@@ -0,0 +1,27 @@
+const yellow = document.getElementById("yellow")
+const blue = document.getElementById("blue")
+const black = document.getElementById("black")
+const grey = document.getElementById("grey")
+const btn = document.querySelector(".btn")
+
+
+
+const products = [
+    {
+        id = 0, 
+        name: "Lillian Be Our Guest Decorative Pillow", 
+        category: "Levtex Home Collection",
+        price: 39.99, 
+        img: "img1.jpeg"
+    },
+    {
+        id = 2, 
+        name: "Overfilled Bed Pillow", 
+        category: "Made By Design Collection",
+        price: 19.99, 
+        img: "img2.png"
+    }
+
+]
+
+
